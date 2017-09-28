264fc41591ea1e863d370bc671430dd577e8a15f
Stiv Kapten <stkapt@gmail.com>
2017-09-28 06:55:42 +0300

    Esimene vaheülesanne

---
+++ IdeaProjects/idk0051/StudentCard/src/student/StudentDatabase.java
@@ -0,0 +1,10 @@
+package student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDatabase {
+    public static List<Student> students = new ArrayList<>();
+
+    students
+}
